96a7b44bd02b4d513d193c8438fffe60bc7a7f16
Dan Gaffey <danieljgaffey@gmail.com>
2018-05-14 20:50:33 -0400

    Initial Commit

---
+++ calendar/calendar.h
@@ -0,0 +1,19 @@
+//
+//  calendar.h
+//  calendar
+//
+//  Created by Daniel Gaffey on 5/14/18.
+//  Copyright © 2018 UnchartedRealms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for calendar.
+FOUNDATION_EXPORT double calendarVersionNumber;
+
+//! Project version string for calendar.
+FOUNDATION_EXPORT const unsigned char calendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <calendar/PublicHeader.h>
+
+
